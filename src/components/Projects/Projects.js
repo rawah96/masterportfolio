c17700640e364171541419ef7684fc8021013153
rawah <rawah@pdx.edu>
2021-01-05 05:52:06 -0800

    almost done with projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,8 +5,12 @@ import Slide from 'react-reveal/Slide';
 import {Link} from 'react-router-dom'
 import react from './react.png'
 import css3 from './css3.png'
-import npm from './npm.png'
 import Flip from 'react-reveal/Flip';
+import html5 from './html5.png'
+import tmdb from './tmdb.jpeg'
+import material from './material.png'
+import redux from './redux.svg'
+import node from './node.png'
 
 function Projects() {
     return (
@@ -20,6 +24,11 @@ function Projects() {
             <Slide right>
                 <div className="right-section">
                     <h1>Projects</h1>
+                    <br />
+                    <br />
+                    <h4 id="projects-h4">Most of my recent projects are focused on the client side using vast variety of latest technology tools. </h4>
+                    <br />
+                    <h4 id="projects-h4">In this portfolio, I am showcasing my Reactjs &amp; CSS skills</h4>
                 </div>
             </Slide>
             </div>
@@ -29,22 +38,253 @@ function Projects() {
                     <h1>This Portfolio</h1>
                     <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
                     {/* <h4 id="tech">Technologies used</h4> */}
-
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
                     <div className="btns">
                         <button><a href="https://github.com/rawah96/masterportfolio" target="_blank"> GitHub</a></button>
-                        <button><Link to="/masterportfolio">Demo</Link></button>
+                        {/* <button><Link to="/masterportfolio">Demo</Link></button> */}
+                        <button><a href="https://rawah96.github.io/masterportfolio/">Demo</a></button>
                     </div>
                 </div>
-                </Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
-                <Flip left><div className="row">1</div></Flip>
 
+                </Flip>
+                <Flip left><div className="row">
+                    <h1>Netflix Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                        <img src={tmdb} alt="tmdb" width="35px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Hulu Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                        <img src={tmdb} alt="tmdb" width="35px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Nike Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Youtube Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using Firebase.</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>                    
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Discord Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                        <img src={redux} alt="redux" width='40px' />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Covid Dashboard 0</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Covid Dashboard 1</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Capstone | EMS</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using Heroku.</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                        <img src={redux} alt="redux" width='40px' />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Mern Store</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using Heroku.</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" /> 
+                        <img src={redux} alt="redux" width='50px' />
+                        <img src={node} alt="node" width="30px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>PORTFOLIO 0</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+
+                <Flip left><div className="row">
+                    <h1>PORTFOLIO 1</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>PORTFOLIO 2</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Facebook Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using Firebase.</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Linkedin Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                        <img src={redux} alt="redux" width='40px' />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
+
+                <Flip left><div className="row">
+                    <h1>Gmail Clone</h1>
+                    <h4 id="row-h4">The complete portfolio website hosted using github pages (gh-pages).</h4>
+                    <div id="techs">
+                        <img src={html5} alt="css3" width="50px"/>
+                        <img src={react} alt="react" width="100px" />
+                        <img src={css3} alt="css3" width="40px"/>
+                        <img src={material} alt="material" width="55px" />
+                        <img src={redux} alt="redux" width='40px' />
+                    </div>
+                    <div className="btns">
+                        <button><a href="" target="_blank"> GitHub</a></button>
+                        <button><a href="" target="_blank">Demo</a></button>
+                    </div>
+                </div></Flip>
             </div>
         </div>
     )
